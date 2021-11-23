5feb8f1e6036e26d382b7f8907077463b554fa19
Fabricio Valadares <fabriciovaladares0@gmail.com>
2021-11-23 13:28:15 -0300

    feature: create sessao

--- src/components/AreaCreateUser/index.tsx
+++ src/components/AreaCreateUser/index.tsx
@@ -1,22 +1,42 @@
 import { Sessao } from "../Sessao";
 import style from "./style.module.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataListSectionContext } from "../../Providers/dataListSection";
+import Modal from "../../components/Modal";
+import { CreateSessao } from "../CreateSessao";
 
 const AreaCreateUser = () => {
   const { sessao } = useContext(DataListSectionContext);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   return (
-    <section className={style.conteiner}>
-      <div className={style.conteinerCreateUser}>
-        <div className={style.createSessao}>
-          <button>Criar Sessão</button>
+    <>
+      <Modal open={openModal} handleClose={handleCloseModal}>
+        <CreateSessao close={handleCloseModal} />
+      </Modal>
+      <section className={style.conteiner}>
+        <div className={style.conteinerCreateUser}>
+          <div className={style.createSessao}>
+            <button onClick={handleOpenModal}>Criar Sessão</button>
+          </div>
+          {sessao.map((element) => (
+            <Sessao
+              key={element.id}
+              title={element.title}
+              card={element.card}
+            />
+          ))}
         </div>
-        {sessao.map((element) => (
-          <Sessao key={element.id} title={element.title} card={element.card} />
-        ))}
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,28 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
 import { Dashboard } from "../components/Dashboard";
 import { api } from "../services/api";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { DataListSectionContext } from "../Providers/dataListSection";
 
-const DashboardPage = (props: any) => {
+export const requestApiListSessao = (setSessao: any) => {
+  api
+    .get("/section/listsectionuser", {
+      headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mzc2NzkzOTksImV4cCI6MTYzNzg1MjE5OSwic3ViIjoiY2MxOGY0NzktMWRlYi00NTI2LTk4MDItYzJmNzczOWM1NzJkIn0.RIqfXbKZFrf8pkbXDJww5VEowRoqqgdYqPJTRcPzNvc`,
+      },
+    })
+    .then((response) => {
+      setSessao(response.data);
+    })
+    .catch((err) => console.log(err));
+};
+
+const DashboardPage = () => {
   const { setSessao } = useContext(DataListSectionContext);
 
-  setSessao(props.data);
+  useEffect(() => {
+    requestApiListSessao(setSessao);
+  }, []);
 
   return (
     <>
@@ -18,23 +32,6 @@ const DashboardPage = (props: any) => {
       <Dashboard />
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await api
-    .get("/section/listsectionuser", {
-      headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mzc2NzkzOTksImV4cCI6MTYzNzg1MjE5OSwic3ViIjoiY2MxOGY0NzktMWRlYi00NTI2LTk4MDItYzJmNzczOWM1NzJkIn0.RIqfXbKZFrf8pkbXDJww5VEowRoqqgdYqPJTRcPzNvc`,
-      },
-    })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => console.log(err));
-
-  return {
-    props: { data },
-  };
 };
 
 export default DashboardPage;
